0efe6dea6c174ee079e4a711deaf1483ec26c04a
abinishifex <abinishifex@gmail.com>
2025-12-15 07:03:05 -0900

    Li change

--- src/App.js
+++ src/App.js
@@ -8,8 +8,12 @@ function App() {
         <h1>This is my portfolio</h1>
         <h2>Welcome to my personal website!</h2>
         <p>This is this the event</p>
-        <ul>what makes me confident</ul>
-        
+        <ul>what makes me confident
+          <li>My family support me</li>
+          <li>I work hard</li>
+          <li>I learn from my mistakes</li>
+        </ul>
+
         </header>
     </div>
   );
