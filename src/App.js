7381906bd40d7f13345fa1f0b49f6ac96af3adef
abinishifex <abinishifex@gmail.com>
2025-12-16 08:25:28 -0900

    mini calculator

--- src/App.js
+++ src/App.js
@@ -1,22 +1,22 @@
-import logo from './logo.svg';
-import './App.css';
-
-function App() {
+import { useState } from "react";
+//mini calculator app
+ function App(){
+  const [input1, setInputA] = useState("");
+  const [input2, setInputB] = useState("");
+  const [res, setRes] = useState(0);
   return (
-    <div className="App">
-      <header className="App-header">
-        <h1>This is my portfolio</h1>
-        <h2>Welcome to my personal website!</h2>
-        <p>This is this the event</p>
-        <ul>what makes me confident
-          <li>My family support me</li>
-          <li>I work hard</li>
-          <li>I learn from my mistakes</li>
-        </ul>
+    <div>
+    <h1>Mini Calculator App</h1>
+      <input  value={input1} onChange={e => setInputA(e.target.value)} />
+      <input  value={input2} onChange={e => setInputB(e.target.value)} />
 
-        </header>
-    </div>
+      <button onClick={() => setRes(Number(input1)+ Number(input2))}>+</button>
+      <button onClick={() => setRes(Number(input1)- Number(input2))}>-</button>
+      <button onClick={() => setRes(Number(input1)* Number(input2))}>*</button>
+      <button onClick={() => setRes(Number(input1)/ Number(input2))}>/</button>
+      <p>Result: {res}</p>
+      </div>
+      
   );
-}
-
-export default App;
+ }
+ export default App;
